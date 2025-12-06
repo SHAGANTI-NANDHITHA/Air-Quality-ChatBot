919ea5ba4f4e561e92b77d05e6153a527f5ee6f2
SHAGANTI NANDHITHA <nandhithashaganti@gmail.com>
2025-12-06 15:52:37 +0530

    addded backend url

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # --- Backend URL ---
-BACKEND_URL = "http://127.0.0.1:8000"
+BACKEND_URL = "https://air-quality-chatbot-1.onrender.com"
 
 st.set_page_config(page_title="Air Quality Chatbot", layout="centered")
 st.title("🌍 Air Quality Health Chatbot")
